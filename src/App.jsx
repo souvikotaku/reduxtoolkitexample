29eb4f7608b4cae2061f1f389dc0f470b1fd71a6
souvikotaku <manson1989@>
2023-11-11 19:18:13 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import UserDetails from './components/UserDetails'
+
+const App = () => {
+  return (
+    <>
+    <div>
+    <Navbar/>
+    <UserDetails/>
+    </div>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/store/slices/UserSlice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: [],
+  reducers: {
+    addUser: (state, action) => {
+      
+    },
+    removeUser:(state, action) => {
+
+    },
+    deleteUser:(state, action) => {
+
+    }
+  },
+});
+
+
+
+export {userSlice};
